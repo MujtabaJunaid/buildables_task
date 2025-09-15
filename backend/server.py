fbcd52490e0dbb1133bc4e58171eb81baa2a4093
MujtabaJunaid <mujtabajunaid412@gmail.com>
2025-09-15 14:42:11 +0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mujtabajunaid.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
